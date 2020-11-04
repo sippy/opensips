669a0b1013dfc1a30a57ca3dbcece8c76e2e70e4
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-03 16:39:59 -0800

    Put opensips_test run into main script section.

--- scripts/build/setup_tap.sh
+++ scripts/build/setup_tap.sh
@@ -6,4 +6,4 @@ set -x
 git clone https://github.com/zorgnax/libtap.git dist/libtap
 make -C dist/libtap CPPFLAGS="${CC_EXTRA_OPTS}" LDFLAGS="${CC_EXTRA_OPTS}" clean all
 sudo make -C dist/libtap install
-rm /usr/local/lib/libtap.so
+sudo rm /usr/local/lib/libtap.so
