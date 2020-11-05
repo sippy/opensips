2aafc08ab3b9978b259b8f73702cbc5bac2ef57e
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-04 17:48:37 -0800

    Take targets as argument.

--- scripts/build/travis_build.sh
+++ scripts/build/travis_build.sh
@@ -5,8 +5,7 @@ set -x
 
 CC_EXTRA_OPTS=${CC_EXTRA_OPTS:-"-Werror -Idist/libtap"}
 LD_EXTRA_OPTS=${LD_EXTRA_OPTS:-"-Ldist/libtap -L/usr/local/lib -lssl -lcrypto"}
-MAKE_TGT=${MAKE_TGT:-"all opensips_test"}
 
 make CC_EXTRA_OPTS="${CC_EXTRA_OPTS}" LD_EXTRA_OPTS="${LD_EXTRA_OPTS}" \
  FASTER=1 NICER=0 exclude_modules="db_oracle osp sngtc cachedb_cassandra \
- cachedb_couchbase cachedb_mongodb auth_jwt" ${MAKE_TGT}
+ cachedb_couchbase cachedb_mongodb auth_jwt" "${@}"
