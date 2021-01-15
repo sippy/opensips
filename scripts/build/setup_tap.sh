08f4bad5067c1671b3ece7a02ffb5fcd65c47252
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-15 10:02:37 -0800

    Include build.conf.sub to setup compiler etc.

--- scripts/build/setup_tap.sh
+++ scripts/build/setup_tap.sh
@@ -3,5 +3,7 @@
 set -e
 set -x
 
+. $(dirname $0)/build.conf.sub
+
 git clone https://github.com/zorgnax/libtap.git dist/libtap
 make -C dist/libtap CPPFLAGS="${CC_EXTRA_OPTS}" LDFLAGS="${CC_EXTRA_OPTS}" clean libtap.a
