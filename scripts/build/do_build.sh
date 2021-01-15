7b149c492916c7c5e9d508f6bfaa7b5c0684a1ce
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-15 10:14:27 -0800

    Build opensips_test always.

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -6,7 +6,7 @@ PKGS=`grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
 
 . $(dirname $0)/build.conf.sub
 
-MAKE_TGT="${MAKE_TGT:-"all opensips_test"}"
+MAKE_TGT="${MAKE_TGT:-"all"} opensips_test"
 CC_EXTRA_OPTS="${CC_EXTRA_OPTS:-"-Werror"} -Idist/libtap"
 
 CC_EXTRA_OPTS="${CC_EXTRA_OPTS}" FASTER=1 NICER=0 make \
