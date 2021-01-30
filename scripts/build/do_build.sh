32a9ef11ca5a7f8947dafc149ebd09643b5fe9ee
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-29 17:36:11 -0800

    Provide full path to tap.h as part of the -I.

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -7,7 +7,7 @@ PKGS=`grep -A 35 packages: .travis.yml  | grep -e '^ *[-]' | awk '{print $2}'`
 . $(dirname $0)/build.conf.sub
 
 MAKE_TGT="${MAKE_TGT:-"all"} opensips_deeptests opensips_unittests"
-CC_EXTRA_OPTS="${CC_EXTRA_OPTS:-"-Werror"} -Idist/libtap"
+CC_EXTRA_OPTS="${CC_EXTRA_OPTS:-"-Werror"} -I`pwd`/dist/libtap"
 
 CC_EXTRA_OPTS="${CC_EXTRA_OPTS}" FASTER=1 NICER=0 make \
   LD_EXTRA_OPTS=-Ldist/libtap exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
