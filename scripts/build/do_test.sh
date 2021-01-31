598a11091c68b0184c1779704589217fb9648b8f
Maksym Sobolyev <sobomax@sippysoft.com>
2021-01-31 10:16:34 -0800

    Only run if the ${mod_name}_unittests.so exists.

--- scripts/build/do_test.sh
+++ scripts/build/do_test.sh
@@ -6,5 +6,9 @@ set -e
 for tcfile in modules/*/test/*.cfg
 do
   mod_name=`echo ${tcfile} | awk -F / '{print $2}'`
+  if [ ! -e modules/${mod_name}/${mod_name}_unittests.so ]
+  then
+    continue
+  fi
   ./opensips_deeptests -T ${mod_name} 2>>opensips_test.elog
 done
