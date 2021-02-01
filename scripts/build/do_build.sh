f0a3ea3c92d1578677d391952024315ea8f615c9
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-01 12:36:56 -0800

    Add experimental patch to try building some extra modules.

--- scripts/build/do_build.sh
+++ scripts/build/do_build.sh
@@ -12,3 +12,15 @@ CC_EXTRA_OPTS="${CC_EXTRA_OPTS:-"-Werror"} -I`pwd`/dist/libtap"
 CC_EXTRA_OPTS="${CC_EXTRA_OPTS}" FASTER=1 NICER=0 make \
   LD_EXTRA_OPTS=-Ldist/libtap exclude_modules="db_oracle osp sngtc cachedb_cassandra cachedb_couchbase \
   cachedb_mongodb auth_jwt" ${MAKE_TGT}
+
+for mmfile in modules/*/Makefile
+do
+  mod_name=`echo ${mmfile} | awk -F / '{print $2}'`
+  if [ -e modules/${mod_name}/${mod_name}.so ]
+  then
+    continue
+  fi
+  CC_EXTRA_OPTS="${CC_EXTRA_OPTS}" FASTER=1 NICER=0 make \
+    LD_EXTRA_OPTS=-Ldist/libtap -C modules/${mod_name} ${mod_name}.so || continue
+  echo "modules/${mod_name}/${mod_name}.so is also buildable!"
+done
