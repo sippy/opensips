sippy/opensips

commit 302058d8d384c2bebdd2f54388989b576a201980

python: port script functions to new param interface

modules/python/python_exec.c
@@ -29,6 +29,7 @@
 #include "../../action.h"
 #include "../../config.h"
 #include "../../parser/parse_uri.h"
+#include "../../ut.h"
 
 #include "python_exec.h"
 #include "python_mod.h"
@@ -36,28 +37,29 @@
 #include "python_support.h"
 
 int
-python_exec1(struct sip_msg* _msg, char* method_name, char *foobar)
-{
-    return python_exec2(_msg, method_name, NULL);
-}
-
-int
-python_exec2(struct sip_msg *_msg, char *method_name, char *mystr)
+python_exec(struct sip_msg *_msg, str *_method_name_s, str *_mystr_s)
 {
     PyObject *pFunc, *pArgs, *pValue, *pResult;
     PyObject *msg;
     int rval;
+    str method_name;
+    str mystr;
+
+    if (pkg_nt_str_dup(&method_name, _method_name_s) < 0)
+        return -1;
+    if (_mystr_s && pkg_nt_str_dup(&mystr, _mystr_s) < 0)
+        return -1;
 
     PyEval_AcquireLock();
     PyThreadState_Swap(myThreadState);
 
-    pFunc = PyObject_GetAttrString(handler_obj, method_name);
+    pFunc = PyObject_GetAttrString(handler_obj, method_name.s);
     if (pFunc == NULL || !PyCallable_Check(pFunc)) {
-        LM_ERR("%s not found or is not callable\n", method_name);
+        LM_ERR("%s not found or is not callable\n", method_name.s);
         Py_XDECREF(pFunc);
         PyThreadState_Swap(NULL);
         PyEval_ReleaseLock();
-        return -1;
+        goto error;
     }
 
     msg = newmsgobject(_msg);
@@ -66,10 +68,10 @@ python_exec2(struct sip_msg *_msg, char *method_name, char *mystr)
         Py_DECREF(pFunc);
         PyThreadState_Swap(NULL);
         PyEval_ReleaseLock();
-        return -1;
+        goto error;
     }
 
-    pArgs = PyTuple_New(mystr == NULL ? 1 : 2);
+    pArgs = PyTuple_New(mystr.s == NULL ? 1 : 2);
     if (pArgs == NULL) {
         LM_ERR("PyTuple_New() has failed\n");
         msg_invalidate(msg);
@@ -77,21 +79,21 @@ python_exec2(struct sip_msg *_msg, char *method_name, char *mystr)
         Py_DECREF(pFunc);
         PyThreadState_Swap(NULL);
         PyEval_ReleaseLock();
-        return -1;
+        goto error;
     }
     PyTuple_SetItem(pArgs, 0, msg);
     /* Tuple steals msg */
 
-    if (mystr != NULL) {
-        pValue = PyString_FromString(mystr);
+    if (_mystr_s != NULL) {
+        pValue = PyString_FromString(mystr.s);
         if (pValue == NULL) {
-            LM_ERR("PyString_FromString(%s) has failed\n", mystr);
+            LM_ERR("PyString_FromString(%s) has failed\n", mystr.s);
             msg_invalidate(msg);
             Py_DECREF(pArgs);
             Py_DECREF(pFunc);
             PyThreadState_Swap(NULL);
             PyEval_ReleaseLock();
-            return -1;
+            goto error;
         }
         PyTuple_SetItem(pArgs, 1, pValue);
         /* Tuple steals pValue */
@@ -103,22 +105,32 @@ python_exec2(struct sip_msg *_msg, char *method_name, char *mystr)
     Py_DECREF(pFunc);
     if (PyErr_Occurred()) {
         Py_XDECREF(pResult);
-        python_handle_exception("python_exec2", method_name);
+        python_handle_exception("python_exec2", method_name.s);
         PyThreadState_Swap(NULL);
         PyEval_ReleaseLock();
-        return -1;
+        goto error;
     }
 
     if (pResult == NULL) {
         LM_ERR("PyObject_CallObject() returned NULL\n");
         PyThreadState_Swap(NULL);
         PyEval_ReleaseLock();
-        return -1;
+        goto error;
     }
 
     rval = PyInt_AsLong(pResult);
     Py_DECREF(pResult);
     PyThreadState_Swap(NULL);
     PyEval_ReleaseLock();
+    
+    pkg_free(method_name.s);
+
     return rval;
+
+error:
+    pkg_free(method_name.s);
+    if (_mystr_s)
+        pkg_free(mystr.s);
+
+    return -1;
 }

modules/python/python_exec.h
@@ -24,7 +24,6 @@
 
 #include "../../parser/msg_parser.h"
 
-int python_exec1(struct sip_msg *, char *, char *);
-int python_exec2(struct sip_msg *, char *, char *);
+int python_exec(struct sip_msg *_msg, str *_method_name_s, str *_mystr_s);
 
 #endif

modules/python/python_mod.c
@@ -54,14 +54,22 @@ static param_export_t params[]={
 /*
  * Exported functions
  */
+// static cmd_export_t cmds[] = {
+//     { "python_exec", (cmd_function)python_exec1, 1,  NULL, 0,
+//       REQUEST_ROUTE | FAILURE_ROUTE
+//       | ONREPLY_ROUTE | BRANCH_ROUTE },
+//     { "python_exec", (cmd_function)python_exec2, 2,  NULL, 0,
+//       REQUEST_ROUTE | FAILURE_ROUTE
+//       | ONREPLY_ROUTE | BRANCH_ROUTE },
+//     { 0, 0, 0, 0, 0, 0 }
+// };
+
 static cmd_export_t cmds[] = {
-    { "python_exec", (cmd_function)python_exec1, 1,  NULL, 0,
-      REQUEST_ROUTE | FAILURE_ROUTE
-      | ONREPLY_ROUTE | BRANCH_ROUTE },
-    { "python_exec", (cmd_function)python_exec2, 2,  NULL, 0,
-      REQUEST_ROUTE | FAILURE_ROUTE
-      | ONREPLY_ROUTE | BRANCH_ROUTE },
-    { 0, 0, 0, 0, 0, 0 }
+    {"python_exec", (cmd_function)python_exec, {
+        {CMD_PARAM_STR,0,0},
+        {CMD_PARAM_STR|CMD_PARAM_OPT,0,0}, {0,0,0}},
+        REQUEST_ROUTE|FAILURE_ROUTE|ONREPLY_ROUTE|BRANCH_ROUTE},
+    {0,0,{{0,0,0}},0}
 };
 
 /** module exports */

modules/python/python_msgobj.c
@@ -193,6 +193,8 @@ msg_getHeader(msgobject *self, PyObject *args)
 static PyObject *
 msg_call_function(msgobject *self, PyObject *args)
 {
+/* TODO: adapt to new cmd_export and params interface */
+#if 0
     int i, rval;
     char *fname, *arg1, *arg2;
     cmd_export_t *fexport;
@@ -216,7 +218,7 @@ msg_call_function(msgobject *self, PyObject *args)
     if(!PyArg_ParseTuple(args, "s|ss:call_function", &fname, &arg1, &arg2))
         return NULL;
 
-    fexport = find_cmd_export_t(fname, i - 1, 0);
+    fexport = find_cmd_export_t(fname, 0);
     if (fexport == NULL) {
         PyErr_SetString(PyExc_RuntimeError, "no such function");
         Py_INCREF(Py_None);
@@ -281,6 +283,9 @@ msg_call_function(msgobject *self, PyObject *args)
     pkg_free(act);
 
     return PyInt_FromLong(rval);
+#endif
+
+    return NULL;
 }
 
 PyDoc_STRVAR(copy_doc,

commit 72740085cb5907569c7ce911bbf427238f5f3f23

python: Don't leak memory in python_exec().
The bug was introduced in rev 302058d8d38 (April 2019) along
with module API cleanup and affects all releases since 3.0.

modules/python/python_exec.c
@@ -47,9 +47,9 @@ python_exec(struct sip_msg *_msg, str *_method_name_s, str *_mystr_s)
     str mystr;
 
     if (pkg_nt_str_dup(&method_name, _method_name_s) < 0)
-        return -1;
+        goto e0;
     if (_mystr_s && pkg_nt_str_dup(&mystr, _mystr_s) < 0)
-        return -1;
+        goto e1;
 
     PyEval_AcquireThread(myThreadState);
 
@@ -115,15 +115,17 @@ python_exec(struct sip_msg *_msg, str *_method_name_s, str *_mystr_s)
     rval = PyLong_AsLong(pResult);
     Py_DECREF(pResult);
     PyEval_ReleaseThread(myThreadState);
-    
+
+    if (_mystr_s)
+        pkg_free(mystr.s);
     pkg_free(method_name.s);
 
     return rval;
 
 error:
-    pkg_free(method_name.s);
     if (_mystr_s)
         pkg_free(mystr.s);
-
+e1: pkg_free(method_name.s);
+e0:
     return -1;
 }